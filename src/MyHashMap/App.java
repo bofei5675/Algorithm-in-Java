fb6eca7060702ce5a336dc38b488226634e6799f
bofei5675 <8196385@qq.com>
2018-05-24 00:40:05 -0400

    linear probing

--- src/MyHashMap/App.java
+++ src/MyHashMap/App.java
@@ -1,5 +1,12 @@
 package MyHashMap;
 
 public class App {
-
+    public static void main(String[] args) {
+        HashTable ht = new HashTable();
+        ht.put(1, 10);
+        ht.put(1, 100);
+        ht.put(3, 1000);
+        ht.put(4, 10000);
+        System.out.println(ht.get(1));
+    }
 }
--- src/MyHashMap/HashTable.java
+++ src/MyHashMap/HashTable.java
@@ -9,15 +9,21 @@ public class HashTable {
         return key % Constants.TABLE_SIZE;
     }
 
+    public HashTable() {
+        this.hashtable = new HashItem[Constants.TABLE_SIZE];
+    }
+
     // put method with O1 complexity
     public void put(int key, int value) {
         int hashIndex = this.hash(key);
         if (this.hashtable[hashIndex] == null) {
+            System.out.println("No collision condition ...");
             this.hashtable[hashIndex] = new HashItem(key, value);
         } else {
             // find the last item in this slot
             // while loop in the original video
-
+            System.out.println(
+                    "Collision condition ... and going to the last item");
             HashItem lastItem = this.findLastItem(this.hashtable[hashIndex]);
             lastItem.setNextItem(new HashItem(key, value));
         }
